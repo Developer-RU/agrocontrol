1e38d1befe501ec418844e2ce59e08d2e988bb15
Masyukov Pavel <Developer-RU@users.noreply.github.com>
2020-06-16 18:32:08 +0500

    Update http_sender.py

--- http_sender.py
+++ http_sender.py
@@ -1,78 +1,56 @@
 #!/usr/bin/python
-# -*- coding: utf-8
+# -*- coding: utf-8 -*-
 
-import MySQLdb
-import string
-import time
-import serial
-import sys, os, gc
-import urlparse
-import requests
-import json
-import random
-import time
-import io
-
+import MySQLdb, time, gc, requests, json, time
 from datetime import datetime
-
-
-
 
 while True :
 
-	
 	try:
 
 		db = MySQLdb.connect("localhost", "root", "23111983", "data")
 		cursor = db.cursor()
 	
-		timestamp = 1545730073
-		dt_object = datetime.fromtimestamp(timestamp)
-
-		#print(dt_object.strftime('%Y-%m-%d %H:%M:%S'))
-
-		sql = "INSERT INTO `db` ( `id`, `timestamp`, `lat`, `lon`, `rfid`, `wet`, `s1`, `s2`, `s3`, `s4` ) VALUES ( NULL, '" + dt_object.strftime('%Y-%m-%d %H:%M:%S') + "', '%s', '%s', '%s', '%d', '%d', '%d', '%d', '%d' ) " %  ( '59.8879318237', '50.8879318237', '0', 20, 123, 234, 345, 456)
+		sql = "SELECT * FROM `db` WHERE `send` < 1  ORDER BY `id` DESC LIMIT 10"
 		cursor.execute(sql)
+		records = cursor.fetchall()
 
-		db.commit()
-		db.close()
-
-
-		rnd = random.randrange(1, 11, 2) 
-	
-		str_json = '{"device":"raspberry1","array":['
-		for n in range(0, rnd) :
-			str_json = str_json + '{"time":"1592156705234","lat":"59.8879318237","lon":"54.589931488","RFID":"0","wet":"0","sensors":{"s1":"0","s2":"0","s3":"0","s4":"0"}}'
-			str_json = str_json + ','
 		
-		str_json = str_json[:-1]
-		str_json = str_json + ']}'
+		if cursor.rowcount  > 0 :
+
+			_id = []
+
+			str_json = '{"device":"raspberry1","array":['
+			for row in records :
+				id, timestamp, lat, lon, rfid, wet, s1, s2, s3, s4, send, backup = row
+				str_json = str_json + '{"time":"' + str(timestamp) + '","lat":"' + str(lat) + '","lon":"' + str(lon) + '","RFID":"' + str(rfid) + '","wet":"' + str(wet) + '","sensors":{"s1":"' + str(s1) + '","s2":"' + str(s2) + '","s3":"' + str(s3) + '","s4":"' + str(s4) + '"}}'
+				str_json = str_json + ','
+
+				_id.append(id)
+
+			str_json = str_json[:-1]
+			str_json = str_json + ']}'
 	
+			dt_json = json.loads(str_json)
 	
-		#str_json = '{"device":"raspberry1","array":[{"time":"1592156705234","lat":"59.8879318237","lon":"54.589931488","RFID":"0","wet":"0","sensors":{"s1":"0","s2":"0","s3":"0","s4":"0"}},{"time":"1592156705234","lat":"59.8879318237","lon":"54.589931488","RFID":"1","wet":"0","sensors":{"s1":"0","s2":"0","s3":"0","s4":"0"}}]}'
-	
-	
-		dt_json = json.loads(str_json)
-	
-	
-		req = requests.post('http://94.181.44.80:8000/json', data = dt_json) 
-	 
-		encoding = req.encoding      	# 'utf-8'
-		status_code = req.status_code   # 200
-		elapsed = req.elapsed      		# datetime.timedelta(0, 1, 666890)
-		url = req.url           		# 'https://tutsplus.com/'
-		content = req.content
-		text = req.text
-		history = req.history      
-		headers = req.headers['Content-Type']
-	
-	
-		print ''
-		print '---------------------------------------------------------------------------------'
-		print text
-		print '---------------------------------------------------------------------------------'
-		print ''
-	
+			req = requests.post('http://94.181.44.80:8000/json', data = dt_json) 
+			status_code = req.status_code   # 200
+
+			if int(status_code) == 200 :
+
+				for row in range(0, len(_id)) :
+
+					sql = "UPDATE `db` SET `send` = '1' WHERE `db`.`id` = %d" % (_id[row])
+					cursor.execute(sql)
+
+				db.commit()
+		
+		else :
+			
+			time.sleep(1)
+
+		db.close()
+		gc.collect()
 
 	except :
 
