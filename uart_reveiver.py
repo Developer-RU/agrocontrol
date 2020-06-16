5ad6a89409c301b5b00291fdb345dbae60b93b0e
Masyukov Pavel <Developer-RU@users.noreply.github.com>
2020-06-16 16:59:03 +0500

    Update uart_reveiver.py

--- uart_reveiver.py
+++ uart_reveiver.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8
 
-import MySQLdb
-import string
-import time
-import serial
-import sys, os, gc
-import urlparse
-import requests
-import json
-import random
-import time
-import io
-
-from datetime import datetime
+import MySQLdb, time, serial, json, time, datetime, gc
 
 while True :
 
@@ -48,7 +36,7 @@ while True :
 					timestamp = obj_json["time"]
 					dt_object = datetime.fromtimestamp(timestamp)
 		
-					sql = "INSERT INTO `db` ( `id`, `timestamp`, `lat`, `lon`, `rfid`, `wet`, `s1`, `s2`, `s3`, `s4` ) VALUES ( NULL, '" + str(dt_object) + "', '" + str(obj_json["gps"][0]) + "', '" + str(obj_json["gps"][1]) + "', '" + str(obj_json["rfid"]) + "', " + str(obj_json["hum"]) + ", " + str(obj_json["dist"][0]) + ", " + str(obj_json["dist"][1]) + ", " + str(obj_json["dist"][2]) + ", " + str(obj_json["dist"][3]) + " ) "
+					sql = "INSERT INTO `db` ( `id`, `timestamp`, `lat`, `lon`, `rfid`, `wet`, `s1`, `s2`, `s3`, `s4`, `send`, `backup`) VALUES ( NULL, '" + str(dt_object) + "', '" + str(obj_json["gps"][0]) + "', '" + str(obj_json["gps"][1]) + "', '" + str(obj_json["rfid"]) + "', " + str(obj_json["hum"]) + ", " + str(obj_json["dist"][0]) + ", " + str(obj_json["dist"][1]) + ", " + str(obj_json["dist"][2]) + ", " + str(obj_json["dist"][3]) + ", 0, 0 ) "
 					cursor.execute(sql)
 	
 					db.commit()
@@ -58,6 +46,7 @@ while True :
 
 					time.sleep(1)
 
+				gc.collect()
 				time.sleep(1)
 
 	except :
